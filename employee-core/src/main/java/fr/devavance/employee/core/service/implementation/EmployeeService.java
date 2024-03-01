28787663d4866ae8b4b0529a9db2f1232b6d9a16
faty00 <lofaty2020@gmail.com>
2024-03-01 16:40:46 +0100

    tp3

--- employee-core/src/main/java/fr/devavance/employee/core/service/implementation/EmployeeService.java
+++ employee-core/src/main/java/fr/devavance/employee/core/service/implementation/EmployeeService.java
@@ -3,6 +3,7 @@ package fr.devavance.employee.core.service.implementation;
 import fr.devavance.employee.core.entity.Employee;
 import fr.devavance.employee.core.repository.IEmployeeRepository;
 import fr.devavance.employee.core.service.IEmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,26 +13,31 @@ public class EmployeeService implements IEmployeeService {
 
     private IEmployeeRepository repository;
 
+
+    @Autowired
+    public EmployeeService(IEmployeeRepository repository) {
+        this.repository = repository;
+    }
+
     @Override
     public List<Employee> findAll() {
-        return null;
+        return (List<Employee>) repository.findAll();
     }
 
     @Override
     public void save(Employee employee) {
-
+        repository.save(employee);
     }
 
     @Override
     public Employee findById(Long id) {
-        return null;
+        return repository.findById(id).orElse(null);
     }
 
     @Override
     public void delete(Long id) {
-
+        repository.deleteById(id);
     }
 
-    // ...
 
 }
--- employee-web/src/main/java/fr/devavance/employee/controller/implementation/EmployeeController.java
+++ employee-web/src/main/java/fr/devavance/employee/controller/implementation/EmployeeController.java
@@ -3,6 +3,7 @@ import java.util.Optional;
 import fr.devavance.employee.core.entity.Employee;
 import fr.devavance.employee.core.repository.IEmployeeRepository;
 import fr.devavance.employee.controller.IEmployeeController;
+import fr.devavance.employee.core.service.IEmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class EmployeeController implements IEmployeeController {
 
     @Autowired
-    private IEmployeeRepository employeeRepository;
+    private IEmployeeService employeeService;
 
 /*
     @RequestMapping("/")
@@ -29,7 +30,7 @@ public class EmployeeController implements IEmployeeController {
 
     @GetMapping("/home")
     public String displayHome(Model model) {
-        model.addAttribute("employees",employeeRepository.findAll());
+        model.addAttribute("employees",employeeService.findAll());
         model.addAttribute("employee", new Employee());
         return "view_home.html";
 
@@ -47,14 +48,14 @@ public class EmployeeController implements IEmployeeController {
     */
     @PostMapping("/addemployee")
     public String addEmployee(Employee employee){
-        employeeRepository.save(employee);
+        employeeService.save(employee);
         return "redirect:/employee/home";
     }
 
     @RequestMapping(value = "/{id}")
     public String displayEmployee(@PathVariable(name = "id") Long id, Model model) {
-        Optional<Employee> employee = employeeRepository.findById(id);
-        model.addAttribute("employee", employee.get());
+        Employee employee = employeeService.findById(id);
+        model.addAttribute("employee", employee);
         return "view_employee";
     }
 }
