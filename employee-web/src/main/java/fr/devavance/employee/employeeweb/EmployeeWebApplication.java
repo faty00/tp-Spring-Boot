4e968fe504c5cc0cc0db731a52dcd167e405d4b0
faty00 <lofaty2020@gmail.com>
2024-03-01 14:36:44 +0100

    tp3

---
+++ employee-web/src/main/java/fr/devavance/employee/employeeweb/EmployeeWebApplication.java
@@ -0,0 +1,18 @@
+package fr.devavance.employee.employeeweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@SpringBootApplication
+@ComponentScan("fr.devavance.employee")
+
+public class EmployeeWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeWebApplication.class, args);
+    }
+
+}
+
